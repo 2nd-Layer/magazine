bc3bed132843695f0aa5e10968f04a69b53228ce
Mark Stopka <mark.stopka@perlur.cloud>
2019-11-18 12:36:22 +0100

    Update docker-entrypoint.sh to initialize the theme.

--- usr/local/bin/docker-entrypoint.sh
+++ usr/local/bin/docker-entrypoint.sh
@@ -1,9 +1,35 @@
 #!/usr/bin/env bash
 set -e
 
+function installWordpressLocalDev {
+
+  wp-cli config create  --dbname=wordpress \
+  --dbuser=root
+  wp-cli core install --url=localhost \
+    --title=Example \
+    --admin_user=admin \
+    --admin_email=info@example.com \
+    --admin_password="admin" \
+    --skip-email
+  wp-cli theme activate magazine_2ndlayer_eu
+}
+
 function activateSupervisordMariaDB {
+  if /usr/bin/supervisord -c /etc/supervisord.conf; then
+    echo "SupervisorD started!"
+  else
+    echo "SupervisorD start failed!"
+  fi
+  supervisorctl stop nginx
+  supervisorctl stop php-fpm
+  supervisorctl start mariadb
   sed s/autostart=false/autostart=true/ -i /etc/supervisord.d/mariadb.ini
   mysql -u root -e 'CREATE DATABASE wordpress;';
+  installWordpressLocalDev
+  kill $(cat /var/run/mariadb/mariadb.pid)
+  supervisorctl stop mariadb
+  supervisorctl shutdown
+  sleep 10
 }
 
 DOCKER_ENV=${DOCKER_ENV:-localdev}
@@ -12,8 +38,6 @@ MARIADB_DIR=${MARIADB_DIR:-"/var/lib/mysql/"}
 
 if [ ${DOCKER_ENV} == "localdev" ]; then
   if [ -d ${MARIADB_DIR} ]; then
-    mysql_install_db --skip-test-db --user=mysql
-    activateSupervisordMariaDB
     if [ -z "$(ls -A ${MARIADB_DIR})" ]; then
       mysql_install_db --skip-test-db --user=mysql
       activateSupervisordMariaDB
